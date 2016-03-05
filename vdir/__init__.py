93a7db89403c8d53fbc1c95e3a54a38072516c3f
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-03-05 21:00:55 +0100

    Preparing for todoman

--- vdir/__init__.py
+++ vdir/__init__.py
@@ -46,6 +46,15 @@ def _generate_href(ident=None, safe=SAFE_UID_CHARS):
         return ident
 
 
+def _get_etag_from_file(fpath):
+    '''Get mtime-based etag from a filepath.'''
+    stat = os.stat(fpath)
+    mtime = getattr(stat, 'st_mtime_ns', None)
+    if mtime is None:
+        mtime = stat.st_mtime
+    return '{:.9f}'.format(mtime)
+
+
 class VdirError(IOError):
     pass
 
@@ -68,7 +77,11 @@ class Item(object):
         self.raw = raw
 
 
-class Vdir(object):
+def _normalize_meta_value(value):
+    return to_native(value or u'').strip()
+
+
+class VdirBase(object):
     item_class = Item
     default_mode = 0o750
 
@@ -76,15 +89,6 @@ class Vdir(object):
         self.path = path
         self.encoding = encoding
         self.fileext = fileext
-
-    @staticmethod
-    def _get_etag_from_file(fpath):
-        '''Get mtime-based etag from a filepath.'''
-        stat = os.stat(fpath)
-        mtime = getattr(stat, 'st_mtime_ns', None)
-        if mtime is None:
-            mtime = stat.st_mtime
-        return '{:.9f}'.format(mtime)
 
     @classmethod
     def discover(cls, path, **kwargs):
@@ -124,14 +128,14 @@ class Vdir(object):
         for fname in os.listdir(self.path):
             fpath = os.path.join(self.path, fname)
             if os.path.isfile(fpath) and fname.endswith(self.fileext):
-                yield fname, self._get_etag_from_file(fpath)
+                yield fname, _get_etag_from_file(fpath)
 
     def get(self, href):
         fpath = self._get_filepath(href)
         try:
             with open(fpath, 'rb') as f:
                 return (Item(f.read().decode(self.encoding)),
-                        self._get_etag_from_file(fpath))
+                        _get_etag_from_file(fpath))
         except IOError as e:
             if e.errno == errno.ENOENT:
                 raise NotFoundError(href)
@@ -165,7 +169,7 @@ class Vdir(object):
         try:
             with atomic_write(fpath, mode='wb', overwrite=False) as f:
                 f.write(item.raw.encode(self.encoding))
-                return fpath, self._get_etag_from_file(f.name)
+                return fpath, _get_etag_from_file(f.name)
         except OSError as e:
             if e.errno == errno.EEXIST:
                 raise AlreadyExists(existing_href=href)
@@ -176,7 +180,7 @@ class Vdir(object):
         fpath = self._get_filepath(href)
         if not os.path.exists(fpath):
             raise NotFoundError(item.uid)
-        actual_etag = self._get_etag_from_file(fpath)
+        actual_etag = _get_etag_from_file(fpath)
         if etag != actual_etag:
             raise WrongEtagError(etag, actual_etag)
 
@@ -185,7 +189,7 @@ class Vdir(object):
 
         with atomic_write(fpath, mode='wb', overwrite=True) as f:
             f.write(item.raw.encode(self.encoding))
-            etag = self._get_etag_from_fileobject(f)
+            etag = _get_etag_from_fileobject(f)
 
         return etag
 
@@ -193,7 +197,7 @@ class Vdir(object):
         fpath = self._get_filepath(href)
         if not os.path.isfile(fpath):
             raise NotFoundError(href)
-        actual_etag = self._get_etag_from_file(fpath)
+        actual_etag = _get_etag_from_file(fpath)
         if etag != actual_etag:
             raise WrongEtagError(etag, actual_etag)
         os.remove(fpath)
@@ -215,3 +219,51 @@ class Vdir(object):
         fpath = os.path.join(self.path, key)
         with atomic_write(fpath, mode='wb', overwrite=True) as f:
             f.write(value.encode(self.encoding))
+
+class Color(object):
+    def __init__(self, x):
+        if not x:
+            raise ValueError('Color is false-ish.')
+        if not x.startswith('#'):
+            raise ValueError('Color must start with a #.')
+        if len(x) != 7:
+            raise ValueError('Color must not have shortcuts. '
+                             '#ffffff instead of #fff')
+        self.raw = x.upper()
+
+    def as_rgb():
+        x = self.raw
+
+        r = x[1:3]
+        g = x[3:5]
+        b = x[5:8]
+
+        if len(r) == len(g) == len(b) == 2:
+            return int(r, 16), int(g, 16), int(b, 16)
+        else:
+            raise ValueError('Unable to parse color value: {}'
+                             .format(self.value)
+
+class ColorMixin(object):
+    color_type = Color
+
+    def get_color(self):
+        try:
+            return self.color_type(self.get_meta('color'))
+        except ValueError:
+            return None
+
+    def set_color(self, value):
+        self.set_meta('color', self.color_type(value).raw)
+
+
+class DisplayNameMixin(object):
+    def get_displayname(self):
+        return self.get_meta('displayname')
+
+    def set_displayname(self, value):
+        self.set_meta('displayname', value)
+
+
+class Vdir(VdirBase, ColorMixin, DisplayNameMixin):
+    pass
